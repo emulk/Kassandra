cc5cfb8158f726fb9ad1e12c841645667000dea9
emulk <orgest.shehaj@gmail.com>
2020-03-22 17:31:54 +0100

    log file name changed in kassandra_history_log

--- src/log/log.php
+++ src/log/log.php
@@ -4,9 +4,8 @@ header('Access-Control-Allow-Origin: *');
 $post_data = $_GET['log'];
 if (!empty($post_data)) {
     $post_data =date("Y-m-d h:i:sa")." ".$post_data ."\n";
-    $filename = 'log.txt';
+    $filename = 'kassandra_history_log.txt';
     $handle = fopen($filename, "a");
-
     fwrite($handle, $post_data);
     fclose($handle);
 }
